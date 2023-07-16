619c35d246c79a3da28738a463feeded18a27468
Abhishek-Singh <abhishekbtp05@gmail.com>
2023-07-17 00:07:11 +0530

    complete the navbar and starting the widget components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,7 +7,12 @@ const Home = () => {
             <Sidebar />
             <div className="homeContainer">
                 <Navbar />
-                home Container
+                <div className="widgets">
+                    <Widget type="user" />
+                    <Widget type="order" />
+                    <Widget type="earning" />
+                    <Widget type="balance" />
+                </div>
             </div>
         </div>
 
